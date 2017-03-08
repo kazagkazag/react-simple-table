9d49067a0c7812bc4405238efb16c36ab55f632c
kazagkazag <kamil@zagrabski.com>
2017-03-08 19:34:31 +0100

    Removed unnecessary files. Generated another index.js for tests.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Table from "./table/Table";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+export default Table;
